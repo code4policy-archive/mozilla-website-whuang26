cc0b0fe7ab337ae8bfdc62b21cbdb9c8a274d6d9
Will Huang <whuang26@gmail.com>
2022-01-11 15:16:45 -0500

    add a toggle on/off button for the Firefox logo, remove Hello world! header

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,20 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
  // Define a Function
  function sayOuch() {
  	alert('Ouch! Stop poking me!');
  }
  
  // Use a CSS selector to identify an element
- var foxImage = document.querySelector('img');
+ var foxImage = document.getElementById("myDIV");
  
  // Assign the function to the onclick event on that element
  foxImage.onclick = sayOuch;
 
-// Add a button called "Cick Me!" to homepage
- document.addEventListener("DOMContentLoaded", function() {
-     var element = document.createElement("button");
-     element.appendChild(document.createTextNode("Click Me!"));
-     var page = document.getElementById("btn");
-     page.appendChild(element);
-     console.log(element);
- });
-
- // Corrected "meta charset="UTF-8"".
+// Add a toggle on/off button to the Firefox logo
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
